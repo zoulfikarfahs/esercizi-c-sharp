7121984da73712b758274699377051a2f395c014
zoulfikarfahs <85038914+zoulfikarfahs@users.noreply.github.com>
2021-12-19 20:34:22 +0100

    Add files via upload

---
+++ pimeNumber2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace primeNumber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+             //in questo loop specifichiamo i margini
+            for(int i = 2; i<=100;i++)
+            {
+                bool isPrimeNumber = true;
+                //in questo loop mettiamo la logica 
+                //per controllare se è prime number o no
+                for(int j=2; j<i; j++ )
+                {
+                     if(i % j == 0)
+                    {
+                         isPrimeNumber = false;
+                         break;
+
+                    }
+                }
+               if(isPrimeNumber)
+               {
+                   Console.Write(i + "  ");
+               }
+                
+
+            } 
+        }
+
+        
+    }
+}
---
+++ primenumber.cs
@@ -0,0 +1,36 @@
+﻿/*Calculate and print all the prime numbers between 1 and 100*/
+
+
+using System;
+
+namespace prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;  
+              
+
+            //Calculate and display the Prime number  
+            Console.WriteLine("Prime Numbers are : "); 
+            //qui viene messo il limite dei numeri che devono essere controllati
+            for (int i = 2; i <= 100; i++) {
+                //questo lupo ci serve per la logica dei numeri primari
+                for (int j = 2; j <= 100; j++) { 
+                    //e dentro l'IF statement verifichiamo se il numero è primo o no
+                    if (i != j && i % j == 0) {  
+                        isPrime = false;  
+                        break;  
+                    }  
+                }  
+
+                if (isPrime) {  
+                Console.Write("\t" + i);  
+            }  
+            isPrime = true;  
+        }  
+        Console.ReadKey();
+        }
+    }
+}
